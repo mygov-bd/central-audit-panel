9046b4146771f4bb514094a83885c4e6e1ef191a
ronykader <rony.ksr06@gmail.com>
2021-12-15 14:51:51 +0600

    update pakcage slug

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,22 +1,22 @@
 <?php return array(
     'root' => array(
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
         'type' => 'package',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => NULL,
-        'name' => 'myov/logtracker',
+        'reference' => 'd84fc53ebd2d413d07f46cc5b7f745bc5aa36b81',
+        'name' => 'mygov/logtracker',
         'dev' => true,
     ),
     'versions' => array(
-        'myov/logtracker' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
+        'mygov/logtracker' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
             'type' => 'package',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => NULL,
+            'reference' => 'd84fc53ebd2d413d07f46cc5b7f745bc5aa36b81',
             'dev_requirement' => false,
         ),
     ),
