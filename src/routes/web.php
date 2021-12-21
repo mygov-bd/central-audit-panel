e8eb89c5abb23ecca1ae3b3c70f1915eb8fc186f
ronykader <rony.ksr06@gmail.com>
2021-12-21 11:39:31 +0600

    update - added route process

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,6 @@
 <?php 
 
-use myGov\Logtracker\Http\Middleware\VerifyLogApiToken;
+// use myGov\Logtracker\Http\Middleware\VerifyLogApiToken;
 use myGov\Logtracker\Http\Controllers\LogtrackerController;
 
 
@@ -8,9 +8,9 @@ use myGov\Logtracker\Http\Controllers\LogtrackerController;
 Route::group(['prefix' => 'api/audit-panel-data'], function () {
     
     /*************Default Logs API******************/
-    Route::get('/', [LogtrackerController::class,'logApidata']);
+    Route::get('/', '\myGov\Logtracker\Http\Controllers\LogtrackerController@logApidata');
 
     /**************Only for MongoDB**************** */
-    Route::get('/log-synchronous', [LogtrackerController::class,'getUnsynchronousData']);
-    Route::post('/log-synchronous', [LogtrackerController::class,'synchronousProcess']);
+    Route::get('/log-synchronous', '\myGov\Logtracker\Http\Controllers\LogtrackerController@getUnsynchronousData');
+    Route::post('/log-synchronous', '\myGov\Logtracker\Http\Controllers\LogtrackerController@synchronousProcess');
 });
